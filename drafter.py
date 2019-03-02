45f3d97e287b0be8df38a3ffa8eb5ae96de9cd69
nmebane <noah.mebane@intel.com>
2019-03-01 17:15:13 -0800

    This version should be able to download all of the projection data and save it to a csv

--- drafter.py
+++ drafter.py
@@ -18,93 +18,101 @@ projection_types = ['steamer']
 
 def setup_intel_proxy():
     """ This is only required when at Intel, to navigate through the proxy """
+
     proxy_dict = {'http' : 'http://proxy-chain.intel.com:911', 'https' : 'http://proxy-chain.intel.com:911'}
     proxy_support = request.ProxyHandler(proxy_dict)
     opener = request.build_opener(proxy_support)
     request.install_opener(opener)
 
-def get_single_player_projection(url, stat_names, projection_db):
+def get_single_player_projection(url, projection_db):
     """ If the URL exists, turn the player's projections into a dict and add the
         dict to the projection DB """
-    try:
-        # Attempt to retrieve the page and turn it into soup
+    while True:
+        try:
+            # Attempt to retrieve the page and turn it into soup
+            response = request.urlopen(url)
+            soup = bs(response, features="lxml")
+
+            player_stats_dict = {}
+
+            # Extract player's name from the soup
+            player_name = soup.find('h1').string
+
+            # Extract player's position
+            position =  soup.find('div', class_='player-info-box-pos').string
+
+            # Extract the stat tables
+            stat_tables = soup.find_all('table', id=re.compile("SeasonStats1_dgSeason[12]_ctl00"))
+            stat_headers_1 = [cell.string for cell in stat_tables[0].find_all('th')]
+            stat_headers_2 = [cell.string for cell in stat_tables[1].find_all('th')]
+            stat_block_1 = stat_tables[0].find_all('tr', class_="grid_projections_show")
+            stat_block_2 = stat_tables[1].find_all('tr', class_="grid_projections_show")
+            num_projections = len(stat_block_1)
+
+            # If this player has projections, add each projection to the DB
+            if (num_projections):
+                for i in range(num_projections):
+                    temp_stat_dict = {"Name" : player_name, "Position" : position}
+                    stat_line_1 = [cell.string for cell in stat_block_1[i].find_all('td')]
+                    stat_line_2 = [cell.string for cell in stat_block_2[i].find_all('td')]
+                    for j in range(len(stat_headers_1)):
+                        temp_stat_dict[stat_headers_1[j]] = stat_line_1[j]
+                    for j in range(len(stat_headers_2)):
+                        temp_stat_dict[stat_headers_2[j]] = stat_line_2[j]
+                    for stat in list(projection_db):
+                        if stat in temp_stat_dict and temp_stat_dict[stat] != '\xa0':
+                            projection_db[stat].append(temp_stat_dict[stat])
+                        else:
+                            projection_db[stat].append(" ")
+        except urllib.error.HTTPError:
+            return
+        except urllib.error.URLError:
+            sleep(10)
+        else:
+            break
+
+def get_stat_names(projection_db):
+    """ Set up the projection DB with the stat field names """
+    
+    projection_db.setdefault('Name', [])
+    projection_db.setdefault('Position', [])
+
+    # Links to a batter and a pitcher since they have different columns
+    batter_stats_url = "https://www.fangraphs.com/statss.aspx?playerid=1"
+    pitcher_stats_url = "https://www.fangraphs.com/statss.aspx?playerid=18"
+
+    for url in batter_stats_url, pitcher_stats_url:
+        # Grab the pages from Fangraphs from which to take the stat names and parse
+        # out the relevant two tables ("Standard" and "Advanced" on the player page)
         response = request.urlopen(url)
         soup = bs(response, features="lxml")
-
-        # Extract player's name from the soup
-        player_name = soup.find('h1').string
-
-        # Extra player's position
-        if soup.find('div', class_='player-info-box-pos').string == 'P':
-            position = 1
-        else:
-            position = 0
-
-        # Extract all of the projections from the soup
-        player_stats = []
-        for i in 1,2:
-            player_stats_temp = []
-            stat_table = soup.find('table', id=re.compile("SeasonStats1_dgSeason{}_ctl00".format(i)))
-            stat_rows = stat_table.find_all('tr', class_=re.compile("grid_projections_show"))
-            for stat_row in stat_rows:
-                stats_list = [cell.string for cell in stat_row.find_all('td')]
-                player_stats_temp.append(stats_list)
-            player_stats.append(player_stats_temp)
-
-        # Put all of the stats into a dict and then add the dict to the
-        # projeciton db
-        player_stats_dict = {}
-        for i in range(2):
-            for stat_row in player_stats[i]:
-                player_stats_dict.setdefault(stat_row[1], {})
-                for j in range(2, len(stat_names[position][i])):
-                    player_stats_dict[stat_row[1]])[stat_names[position][i][j]] = stat_row[j]
-
-        if player_stats_dict:
-            projection_db[player_name] = player_stats_dict
-            print("Added {}".format(player_name))
-
-    except urllib.error.HTTPError:
-        return
-            
-
-def get_stat_names(url):
-    """ Create a list with field names for use in creating dicts later """
-    # Grab the page from Fangraphs from which to take the stat names and parse
-    # out the relevant two tables ("Standard" and "Advanced" on the player page)
-    response = request.urlopen(url)
-    soup = bs(response, features="lxml")
-    stat_tables = soup.find_all('table', id=re.compile("SeasonStats1_dgSeason[12]_ctl00"))
-    # Take all of the table headers and put them into two lists
-    stat_names = []
-    for stat_table in stat_tables[0], stat_tables[1]:
-        stat_names_subset = []
-        stat_names_subset.extend(cell.string for cell in stat_table.find_all('th'))
-        stat_names.append(stat_names_subset) 
-    return stat_names
+        stat_tables = soup.find_all('table', id=re.compile("SeasonStats1_dgSeason[12]_ctl00"))
+        # Create keys in the dict for each stat name
+        for stat_table in stat_tables:
+            for stat_name in [cell.string for cell in stat_table.find_all('th')]:
+                projection_db.setdefault(stat_name, [])
 
 def get_fangraphs_projections():
+    """ Retrieve the projection data from FanGraphs, player by player """
+
     # If behind Intel proxy server, set up proxy support
     try:
         requests.get("http://www.google.com")
     except requests.exceptions.ProxyError:
         setup_intel_proxy()
     
-    # Set the generic part of the URL to request for each player
-    url_prefix = "https://www.fangraphs.com/statss.aspx?playerid="
-    batter_stats_url = "https://www.fangraphs.com/statss.aspx?playerid=1"
-    pitcher_stats_url = "https://www.fangraphs.com/statss.aspx?playerid=18"
-    # Get a list of stat names
-    batter_stat_names = get_stat_names(batter_stats_url)
-    pitcher_stat_names = get_stat_names(pitcher_stats_url)
-    stat_names = [batter_stat_names,pitcher_stat_names]
-    # Retrieve all of the available data for each player
     projection_db = {}
-    for i in range(15490,15510):
+    # Generate keys for each stat name
+    get_stat_names(projection_db)
+    # Retrieve all of the available data for each player
+    url_prefix = "https://www.fangraphs.com/statss.aspx?playerid="
+    for i in [15502,11579,11493,3543]:
         url = "{}{}".format(url_prefix,i)
-        get_single_player_projection(url, stat_names, projection_db)
+        get_single_player_projection(url, projection_db)
         time.sleep(10)
 
+    df = pd.DataFrame(projection_db)
+    df.to_csv(r"c:\\users\\nmebane\\desktop\\projection_db.txt")
     return projection_db
 
 if __name__ == "__main__":
